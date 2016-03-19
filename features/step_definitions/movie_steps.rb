525416502ec2b6cccd5263b4875e605b3aad4bf3
a17100045 <17100045@lums.edu.pk>
2016-03-19 15:09:15 +0000

    first commit

---
+++ features/step_definitions/movie_steps.rb
@@ -0,0 +1,52 @@
+# Add a declarative step here for populating the DB with movies.
+
+Given /the following movies exist/ do |movies_table|
+  movies_table.hashes.each do |movie|
+    # each returned element will be a hash whose key is the table header.
+    # you should arrange to add that movie to the database here.
+    Movie.create!(movie)
+  end
+  #flunk "Unimplemented"
+end
+
+# Make sure that one string (regexp) occurs before or after another one
+#   on the same page
+
+Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
+  #  ensure that that e1 occurs before e2.
+  #  page.body is the entire content of the page as a string.
+ # flunk "Unimplemented"
+end
+
+# Make it easier to express checking or unchecking several boxes at once
+#  "When I uncheck the following ratings: PG, G, R"
+#  "When I check the following ratings: G"
+
+When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+  # HINT: use String#split to split up the rating_list, then
+  #   iterate over the ratings and reuse the "When I check..." or
+  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  #flunk "Unimplemented"
+  puts(rating_list) 
+  puts("HERE")
+  rating_list.split(/,\s*/).each do |rat|
+    rat = "ratings_#{rat}"
+    if(uncheck)
+      uncheck(rat)
+    else
+      check(rat)
+    end
+  end  
+end
+
+Then /I should see all the movies/ do
+  movies_list = page.all('table#movies tr').count
+  assert movies_list==11
+  # Make sure that all the movies in the app are visible in the table
+  #flunk "Unimplemented"
+end
+Then /I should see movies with ratings: (.*)/ do |ratings|
+  ratings.split(/,\s*/).each do |rating|
+    assert page.has_xpath?('//td', :text => "#{rating}")
+  end
+end
